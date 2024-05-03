b037ffb36a9fcc83c5a395e3421aabf3c95c0f73
Prajith Shetty <prajithshettyj@gmail.com>
2024-05-03 09:11:27 +0530

    Restaurant Repo added

---
+++ FoodToOrder_Backend/Repositories/RestaurantRepository.cs
@@ -0,0 +1,38 @@
+﻿using FoodToOrder_Backend.Models;
+
+namespace FoodToOrder_Backend.Repositories
+{
+    public class RestaurantRepository : IRestaurantRepository
+    {
+        
+
+        public void Dispose()
+        {
+            Console.WriteLine("Restaurant Repo Disposed");
+        }
+
+        public Restaurant GetRestaurantById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Restaurant> GetRestaurants()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Restaurant InsertRestaurant(Restaurant restaurant)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Restaurant UpdateRestaurant(Restaurant newRestaurant)
+        {
+            throw new NotImplementedException();
+        }
+        public Restaurant DeleteRestaurant(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
